ed65514e502c659051feb66cca9a8cf86e461e32
1jawsf1 <135722434+1jawsf1@users.noreply.github.com>
2023-06-13 15:51:35 +0800

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+function scrollToSection(issue) {
+    var element = document.getElementById(issue);
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function scrollToSection(info) {
+    var element = document.getElementById(info);
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function scrollToSection(testi) {
+    var element = document.getElementById(testi);
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function scrollToSection(info) {
+    var element = document.getElementById(info);
+    element.scrollIntoView({behavior: "smooth"});
+}
+
+function scrollToSection(prob) {
+    var element = document.getElementById(prob);
+    element.scrollIntoView({behavior: "smooth"});
+}
+function scrollToSection(abouts) {
+    var element = document.getElementById(abouts);
+    element.scrollIntoView({behavior: "smooth"});
+}
